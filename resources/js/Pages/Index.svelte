a3acd1e28d454d3eeef464cc1c9aec3d081c39b7
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-04-14 10:10:03 +0700

    feat:dropdown frame

--- resources/js/Pages/Index.svelte
+++ resources/js/Pages/Index.svelte
@@ -1,37 +1,42 @@
 <script>
-    import { onMount } from "svelte";
+    import { onMount, tick } from "svelte";
     import { fade } from "svelte/transition";
     import { flip } from "svelte/animate";
+    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
     import HomeSkeleton from "../Shared/Skeletons/HomeSkeleton.svelte";
     import SutterSoundFile from "../../assets/sounds/camera-shutter-199580.mp3";
     import ClockSoundFile from "../../assets/sounds/slow-cinematic-clock-ticking-tension-323080.mp3";
+    import { faCamera } from "@fortawesome/free-solid-svg-icons";
 
     let loading = true;
 
-    let errorMessage = '';
+    let errorMessage = "";
 
     let video;
     let canvas;
     let images = [];
 
-    let frames = [1, 2, 3, 4];
-    let selectedFrame = 1;
-
-    $: if (selectedFrame !== null) {
-        console.log("Frame yang dipilih:", selectedFrame);
-    }
+    let photoLayouts = [
+        { label: "2 Foto (Horizontal)", count: 2 },
+        { label: "3 Foto (Vertical)", count: 3 },
+        { label: "4 Foto (Vertical)", count: 4 },
+        { label: "4 Foto (2x2 Grid)", count: 4 },
+    ];
+    let selectedLayout = photoLayouts[0];
+    $: maxImages = selectedLayout.count;
 
     function startCamera() {
         navigator.mediaDevices
             .getUserMedia({ video: true, audio: false })
             .then((stream) => {
+                console.log("Stream berhasil:", stream);
                 video.srcObject = stream;
                 video.play();
-                errorMessage = '';
+                errorMessage = "";
             })
             .catch((err) => {
                 console.error("Gagal mengakses kamera:", err);
-                errorMessage = 'Kamera tidak tersedia atau akses ditolak.';
+                errorMessage = "Kamera tidak tersedia atau akses ditolak.";
             });
     }
 
@@ -46,45 +51,119 @@
     let shutterSound = new Audio(SutterSoundFile);
     let countdownSound = new Audio(ClockSoundFile);
 
-    async function captureImage() {
-        showCountdown = true;
-
-        // 🔁 Hitung mundur
-        for (let i = defaultCountdown; i > 0; i--) {
-            currentCountdown = i;
-
-            // 🔊 Atur ulang dan mainkan suara countdown dari awal
-            countdownSound.pause();
-            countdownSound.currentTime = 0;
-            countdownSound.play();
-
-            await new Promise((res) => setTimeout(res, 1000));
+    function playSound(audio) {
+        audio.pause();
+        audio.currentTime = 0;
+        try {
+            audio.play();
+        } catch (e) {
+            console.warn("Gagal memutar audio", e);
         }
-
-        // 🛑 Hentikan suara countdown saat selesai
-        countdownSound.pause();
-        countdownSound.currentTime = 0;
-
-        showCountdown = false;
-
-        // 🔊 Suara shutter
-        shutterSound.currentTime = 0;
-        await shutterSound.play();
-
-        // 📸 Ambil gambar
-        const context = canvas.getContext("2d");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        context.drawImage(video, 0, 0);
-
-        const img = new Image();
-        img.src = canvas.toDataURL("image/png");
-        images = [...images, img.src].slice(-4);
     }
 
-    onMount(() => {
-        setTimeout(() => {
+    async function captureImage(indexToReplace = null) {
+        if (typeof indexToReplace !== "number") {
+            indexToReplace = null;
+        }
+        console.log("Memulai proses pengambilan gambar");
+        if (indexToReplace === null && images.length > 0) {
+            const confirmClear = window.confirm(
+                "Foto yang sudah diambil akan hilang. Lanjutkan?",
+            );
+            console.log(
+                "Konfirmasi untuk menghapus gambar yang ada:",
+                confirmClear,
+            );
+            if (!confirmClear) return;
+            images = []; // reset gambar
+            console.log("Gambar direset");
+        }
+
+        // Pastikan canvas dan video ada dan terpasang
+        if (!canvas || !video) {
+            console.error("Canvas atau Video tidak ditemukan");
+            return;
+        }
+
+        // Jika ada indexToReplace, kita hanya akan mengganti gambar pada index tersebut
+        if (indexToReplace !== null) {
+            console.log(`Mengganti gambar pada indeks: ${indexToReplace}`);
+            showCountdown = true;
+
+            for (let j = defaultCountdown; j > 0; j--) {
+                currentCountdown = j;
+                console.log("Countdown:", currentCountdown);
+                playSound(countdownSound);
+                await new Promise((res) => setTimeout(res, 1000));
+            }
+
+            countdownSound.pause();
+            countdownSound.currentTime = 0;
+            showCountdown = false;
+
+            playSound(shutterSound);
+
+            const context = canvas.getContext("2d");
+            if (!context) {
+                console.error("Gagal mendapatkan context canvas");
+                return;
+            }
+
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            context.drawImage(video, 0, 0);
+
+            const newImage = canvas.toDataURL("image/png");
+
+            // Ganti gambar pada indeks tertentu
+            console.log("Gambar baru yang diambil:", newImage);
+            images[indexToReplace] = newImage;
+        } else {
+            console.log("Proses mengambil gambar baru dimulai...");
+            // Proses untuk mengambil gambar baru jika tidak ada indexToReplace
+            for (let i = 0; i < maxImages; i++) {
+                console.log(`Mengambil gambar ${i + 1} dari ${maxImages}`);
+                showCountdown = true;
+
+                for (let j = defaultCountdown; j > 0; j--) {
+                    currentCountdown = j;
+                    console.log("Countdown:", currentCountdown);
+                    playSound(countdownSound);
+                    await new Promise((res) => setTimeout(res, 1000));
+                }
+
+                countdownSound.pause();
+                countdownSound.currentTime = 0;
+                showCountdown = false;
+
+                playSound(shutterSound);
+
+                const context = canvas.getContext("2d");
+                if (!context) {
+                    console.error("Gagal mendapatkan context canvas");
+                    return;
+                }
+
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                context.drawImage(video, 0, 0);
+
+                const newImage = canvas.toDataURL("image/png");
+
+                // Tambahkan gambar ke array
+                console.log("Captured image:", newImage);
+                images = [...images, newImage]; // tambahkan gambar ke array
+
+                // Tambahkan jeda antar foto (misalnya 500ms) sebelum mengambil foto berikutnya
+                await new Promise((res) => setTimeout(res, 500));
+            }
+        }
+    }
+
+    onMount(async () => {
+        setTimeout(async () => {
             loading = false;
+            await tick();
             startCamera();
         }, 1500);
         return () => {
@@ -95,24 +174,28 @@
     });
 </script>
 
-<div class="min-h-screen bg-gradient-to-br from-purple-500 via-pink-400 to-blue-500 flex flex-col items-center justify-center p-6 text-white font-sans">
+<div
+    class="min-h-screen bg-gradient-to-br from-purple-500 via-pink-400 to-blue-500 flex flex-col items-center justify-center p-6 text-white font-sans"
+>
     {#if loading}
         <HomeSkeleton />
     {:else}
-        <h1 class="text-3xl md:text-4xl font-bold mb-6 text-center tracking-wide drop-shadow">
+        <h1
+            class="text-3xl md:text-4xl font-bold mb-6 text-center tracking-wide drop-shadow"
+        >
             📸 Photobooth
         </h1>
 
         <div class="flex justify-start items-start gap-4 mb-4">
-            <!-- Dropdown Frame -->
+            <!-- Ganti dropdown frame -->
             <select
-                id="frame"
-                name="frame"
-                bind:value={selectedFrame}
+                id="layout"
+                name="layout"
+                bind:value={selectedLayout}
                 class="block w-full rounded border-gray-300 shadow-sm focus:ring-amber-500 focus:border-amber-500 h-10 bg-white text-black"
             >
-                {#each frames as frame}
-                    <option value={frame}>Frame {frame}</option>
+                {#each photoLayouts as layout}
+                    <option value={layout}>{layout.label}</option>
                 {/each}
             </select>
 
@@ -129,9 +212,12 @@
             </select>
         </div>
 
-        <div class="flex flex-col md:flex-row gap-8 w-full max-w-5xl">
+        <div class="grid grid-cols-5 gap-4 w-full max-w-7xl mx-auto">
+            <!-- Spacer kiri -->
+            <div></div>
+
             <!-- Kamera -->
-            <div class="flex-1 space-y-4">
+            <div class="col-span-3 space-y-4">
                 <div class="relative w-full aspect-video">
                     <video
                         bind:this={video}
@@ -159,7 +245,8 @@
                     {/if}
                 </div>
 
-                <canvas bind:this={canvas} class="hidden"></canvas>
+                <canvas bind:this={canvas} aria-hidden="true" class="hidden"
+                ></canvas>
 
                 <div class="flex justify-center">
                     <button
@@ -172,28 +259,35 @@
             </div>
 
             <!-- Galeri Foto -->
-            <div
-                class={images.length === 0
-                    ? "hidden"
-                    : "flex-1 transition-width duration-300 ease"}
-            >
-                <div
-                    class={images.length === 0
-                        ? "hidden"
-                        : "grid grid-cols-1 gap-4 bg-white/30 backdrop-blur-md rounded-xl p-4 shadow-xl border border-white/40"}
-                >
-                    {#each images as img (img)}
-                        <div animate:flip>
-                            <img
-                                src={img}
-                                alt="Captured"
-                                class="w-1/2 mx-auto rounded-lg shadow-lg transform transition duration-500 hover:scale-105 hover:shadow-2xl"
-                                transition:fade={{ duration: 500 }}
-                            />
-                        </div>
-                    {/each}
+            {#if images.length > 0}
+                <div class="space-y-4 transition-width duration-300">
+                    <div
+                        class="grid grid-cols-1 gap-4 bg-white/30 backdrop-blur-md rounded-xl p-4 shadow-xl border border-white/40"
+                    >
+                        {#each images as img, index (img)}
+                            <div animate:flip class="relative group">
+                                <img
+                                    src={img}
+                                    alt="Captured"
+                                    class="w-1/2 mx-auto rounded-lg shadow-lg hover:shadow-2xl"
+                                    transition:fade={{ duration: 500 }}
+                                />
+
+                                <!-- Wrapper absolute tengah -->
+                                <button
+                                    on:click={captureImage(index)}
+                                    class="absolute cursor-pointer inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black/30 rounded-lg"
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faCamera}
+                                        class="text-white text-2xl"
+                                    />
+                                </button>
+                            </div>
+                        {/each}
+                    </div>
                 </div>
-            </div>
+            {/if}
         </div>
     {/if}
 </div>
