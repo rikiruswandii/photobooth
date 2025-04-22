4b4065502de68b8e905565430063cda8800156d1
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-04-22 14:39:00 +0700

    chore(favicon): update favicon for better branding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon" href="{{ asset('assets/favicon/photobooth.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
